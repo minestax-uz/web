b151a63bb29ac99834f6a294ef0ff76a13d910dd
Sultonov557 <sultonovzerifboy@gmail.com>
2025-05-10 14:11:45 +0500

    env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Define the API URL
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL ||"http://localhost:3000";
 
 // Create an axios instance
 const api = axios.create({
